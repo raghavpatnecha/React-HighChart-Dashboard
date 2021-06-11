f805a8d3bd887db753d8b0f6ef8586ab924f8681
Raghav Patnecha <raghavpatnecha06@gmail.com>
2021-06-11 15:47:58 +0530

    remove comments

--- server/index.js
+++ server/index.js
@@ -9,16 +9,13 @@ import advertisementSpentRoutes from "./routes/advertisementSpent.js";
 
 const app = express();
 
-// below three are middlewares
-app.use(cors()); //api hit access by any url address
-app.use(express.json()); // api calls convert to json format
+app.use(cors()); 
+app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
 app.use('/api/advspentroutes', advertisementSpentRoutes);
-//app.get("/", (req, res)=> { 
-//    return res.status(200).send("welcome to api")
-//});
 
-app.get("/", (req, res)=>                       //arrow functions
+
+app.get("/", (req, res)=>                       
     res.status(200).send("welcome to api")
 );
 
@@ -28,8 +25,5 @@ const MONGO_URI  = process.env.MONGO_URI ;
 mongoose.connect(MONGO_URI , {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => app.listen(5000, () => console.log('Server running on http://localhost:5000')))
     .catch(err => console.log(err));
-//app.listen(5000, () => console.log("hello"));
 
 
-
-//callback - function ke complete hone ke baad koi dusre function ko call karna
